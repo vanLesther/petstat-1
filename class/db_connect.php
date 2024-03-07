09e52ccd6f743dc246630fdb98e1aa824dd80165
Van Lesther Farol <139748960+vanLesther@users.noreply.github.com>
2024-03-08 01:26:04 +0800

    Update db_connect.php

--- class/db_connect.php
+++ class/db_connect.php
@@ -1,11 +1,14 @@
 <?php
-$servername = "petstat-server.mysql.database.azure.com";
-$username = "pgwsyvsiti";
-$password = "455SGCBRI15EHBWB$";
-$dbname = "petstat-server";
+// $servername = "petstat-server.mysql.database.azure.com";
+// $username = "pgwsyvsiti";
+// $password = "455SGCBRI15EHBWB$";
+// $dbname = "petstat-server";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "{path to CA cert}", NULL, NULL);
+mysqli_real_connect($conn, "petstat-server.mysql.database.azure.com", "pgwsyvsiti", "455SGCBRI15EHBWB$", "petstat-server", 3306, MYSQLI_CLIENT_SSL);
 
 // Check connection
 if ($conn->connect_error) {
