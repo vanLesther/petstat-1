c69e036181cc4b0db3c638099b5afbfcaabe8703
Van Lesther Farol <139748960+vanLesther@users.noreply.github.com>
2024-03-09 10:01:53 +0800

    for phpmyadmin dashboard

---
+++ config.inc.php
@@ -0,0 +1,4 @@
+$cfg['Servers'][$i]['host'] = 'petstatvan.mysql.database.azure.com';
+$cfg['Servers'][$i]['port'] = '3306'; // Change if your MySQL server is using a different port
+$cfg['Servers'][$i]['user'] = 'van';
+$cfg['Servers'][$i]['password'] = 'As@dawe123';
